bf3ff8e2f8d63056342bc72fee68627f1d83a2a4
Tapish <narwal83@hzdr.de>
2026-03-11 10:19:17 +0100

    remove namespace

--- tests/spearhed/unit/PushParticles/ValidatePush.hpp
+++ tests/spearhed/unit/PushParticles/ValidatePush.hpp
@@ -33,78 +33,74 @@
 
 #include <cstdint>
 
-namespace spearhed
+/** Kernel that checks postion is 1 from all frames in assigned particle regions
+ * Each block processes all frames in a region and outputs partial sum
+ */
+struct CheckParticlePos
 {
-    /** Kernel that checks postion is 1 from all frames in assigned particle regions
-     * Each block processes all frames in a region and outputs partial sum
-     */
-    struct CheckParticlePos
+    template<typename T_Worker, typename T_PRBox>
+    HDINLINE constexpr auto operator()(T_Worker const& worker, T_PRBox prDeviceBox, int numParticleRegions) const
     {
-        template<typename T_Worker, typename T_PRBox>
-        HDINLINE constexpr auto operator()(T_Worker const& worker, T_PRBox prDeviceBox, int numParticleRegions) const
+        auto const blockIdx = worker.blockDomIdx();
+        // Process all particle regions
+        // TODO optimize work distribution for load balancing
+        for(int regionIdx = blockIdx; regionIdx < numParticleRegions; regionIdx += worker.gridDomSize())
         {
-            auto const blockIdx = worker.blockDomIdx();
-            // Process all particle regions
-            // TODO optimize work distribution for load balancing
-            for(int regionIdx = blockIdx; regionIdx < numParticleRegions; regionIdx += worker.gridDomSize())
+            auto& frameList = prDeviceBox[regionIdx].particleFrameList;
+
+            using FrameType = typename std::remove_cvref_t<decltype(frameList)>::FrameType;
+            constexpr uint32_t frameSize = FrameType::frameSize;
+            // Iterate through all frames
+            for(auto frameItr = frameList.begin(); frameItr != frameList.end(); ++frameItr)
             {
-                auto& frameList = prDeviceBox[regionIdx].particleFrameList;
+                auto forEachSlotInFrame = pmacc::lockstep::makeForEach<frameSize>(worker);
 
-                using FrameType = typename std::remove_cvref_t<decltype(frameList)>::FrameType;
-                constexpr uint32_t frameSize = FrameType::frameSize;
-                // Iterate through all frames
-                for(auto frameItr = frameList.begin(); frameItr != frameList.end(); ++frameItr)
-                {
-                    auto forEachSlotInFrame = pmacc::lockstep::makeForEach<frameSize>(worker);
+                // Each thread processes particles in the frame
+                // TODO unroll last iteration and remove if multimask from the others, if we assume no gaps
+                // use getNumParLastFrame instead of multimask
+                forEachSlotInFrame(
+                    [&](uint32_t const idx)
+                    {
+                        auto particle = (*frameItr)[idx];
 
-                    // Each thread processes particles in the frame
-                    // TODO unroll last iteration and remove if multimask from the others, if we assume no gaps
-                    // use getNumParLastFrame instead of multimask
-                    forEachSlotInFrame(
-                        [&](uint32_t const idx)
+                        // Only sum valid particles
+                        if(*particle[spearhed::multiMask])
                         {
-                            auto particle = (*frameItr)[idx];
-
-                            // Only sum valid particles
-                            if(*particle[multiMask])
+                            if(!particle[spearhed::pos].get().isApprox(1.f))
                             {
-                                if(!particle[pos].get().isApprox(1.f))
-                                {
-                                    printf(
-                                        "particle position incorrect. Got pos x %g vel x %g\n",
-                                        particle[pos].get().get_x(),
-                                        *particle[vel][x]);
-                                }
-                                else
-                                {
-                                    printf("true\n");
-                                }
+                                printf(
+                                    "particle position incorrect. Got pos x %g vel x %g\n",
+                                    particle[spearhed::pos].get().get_x(),
+                                    *particle[spearhed::vel][spearhed::x]);
                             }
-                        });
-                }
+                            else
+                            {
+                                printf("true\n");
+                            }
+                        }
+                    });
             }
         }
-    };
+    }
+};
 
-    /** Computes sum of all particle IDs in the simulation
-     * Uses a parallelisation strategy of using one block per particle region
-     *
-     * @param prBuf Buffer containing all particle regions
-     * @return Sum of all particle IDs
-     */
-    struct ValidatePush
+/** Computes sum of all particle IDs in the simulation
+ * Uses a parallelisation strategy of using one block per particle region
+ *
+ * @param prBuf Buffer containing all particle regions
+ * @return Sum of all particle IDs
+ */
+struct ValidatePush
+{
+    auto operator()() const -> void
     {
-        auto operator()() const -> void
-        {
-            constexpr int numBlocks = 256;
-            constexpr uint32_t threadsPerBlock = 256;
+        constexpr int numBlocks = 256;
+        constexpr uint32_t threadsPerBlock = 256;
 
-            auto& dc = pmacc::Environment<>::get().DataConnector();
-            auto& prBuf = *dc.get<pmacc::spearhed::ParticleRegionBuffer<PRType>>("PRBuf");
+        auto& dc = pmacc::Environment<>::get().DataConnector();
+        auto& prBuf = *dc.get<pmacc::spearhed::ParticleRegionBuffer<spearhed::PRType>>("PRBuf");
 
-            PMACC_LOCKSTEP_KERNEL(CheckParticlePos{})
-                .config<threadsPerBlock>(pmacc::DataSpace<DIM1>(numBlocks))(prBuf.getDeviceDataBox(), prBuf.size);
-        }
-    };
-
-} // namespace spearhed
+        PMACC_LOCKSTEP_KERNEL(CheckParticlePos{})
+            .config<threadsPerBlock>(pmacc::DataSpace<DIM1>(numBlocks))(prBuf.getDeviceDataBox(), prBuf.size);
+    }
+};
