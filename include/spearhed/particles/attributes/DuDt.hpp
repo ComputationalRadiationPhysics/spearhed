8f8429db4ee22d7c1bae6f32f75b397dcba9c117
Tapish <narwal83@hzdr.de>
2026-04-28 14:49:59 +0200

    add attributes for energy and momentum

---
+++ include/spearhed/particles/attributes/DuDt.hpp
@@ -0,0 +1,35 @@
+/* Copyright 2025-2026 Tapish Narwal
+ *
+ * This file is part of SPEARHED.
+ *
+ * SPEARHED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPEARHED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SPEARHED.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <llamaLite/llamaLite.hpp>
+
+namespace spearhed
+{
+    namespace tags
+    {
+        DEFINE_TAG(dudt);
+
+        template<typename T>
+        using dudtField = ll::Field<dudt_t, T>;
+    } // namespace tags
+
+} // namespace spearhed
