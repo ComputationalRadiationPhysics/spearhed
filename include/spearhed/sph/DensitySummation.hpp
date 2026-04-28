d59a648a28eb175593db891685a2f52909775aa8
Tapish <narwal83@hzdr.de>
2026-04-28 14:24:58 +0200

    add density calculation and edit particle particle interaction interface

---
+++ include/spearhed/sph/DensitySummation.hpp
@@ -0,0 +1,111 @@
+/* Copyright 2025-2026 Tapish Narwal
+ *
+ * This file is part of SPEARHED.
+ *
+ * SPEARHED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPEARHED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SPEARHED.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "spearhed/param.hpp"
+#include "spearhed/particles/attributes/Density.hpp"
+#include "spearhed/particles/attributes/Mass.hpp"
+#include "spearhed/particles/attributes/SmoothingLength.hpp"
+#include "spearhed/sph/SphKernel.hpp"
+#include "spmacc/particles/algorithms/ForEachParticle.hpp"
+#include "spmacc/particles/algorithms/ParticleParticleInteraction.hpp"
+
+#include <pmacc/attribute/FunctionSpecifier.hpp>
+
+#include <llamaLite/tag/TagPath.hpp>
+
+namespace spearhed
+{
+    /**
+     * Pairwise interaction functor for SPH density summation.
+     *
+     * Accumulates the neighbour contribution into ownParticle's density:
+     *   rho_i += m_j * W(r, h_i)
+     *
+     * r and is_self are provided by InteractParticles; self-contribution
+     * is seeded by DensityInitSelf before the pairwise pass.
+     */
+    template<SphKernel KernelT>
+    struct AccumulateDensity
+    {
+        using RequiredSharedTags = ll::TagList<tags::mass, tags::smoothingLength>;
+
+        HDINLINE constexpr void operator()(
+            auto& /*worker*/,
+            auto& ownParticle,
+            auto& neighbourParticle,
+            typename CS::T_Axis const r,
+            bool is_self) const
+        {
+            using namespace spearhed::tags;
+
+            if(is_self)
+                return;
+
+            typename CS::T_Axis const h = *ownParticle[smoothingLength];
+            typename CS::T_Axis const m_j = *neighbourParticle[mass];
+
+            *ownParticle[density] += m_j * KernelT::W(r, h);
+        }
+    };
+
+    /**
+     * Per-particle functor: resets density to zero then adds the self-contribution
+     *   rho_i = m_i * W(0, h_i).
+     *
+     * Run this BEFORE the pairwise pass so that densities start from the self term.
+     */
+    template<SphKernel KernelT>
+    struct DensityInitSelf
+    {
+        HDINLINE constexpr void operator()(auto& /*worker*/, auto& particle) const
+        {
+            using namespace spearhed::tags;
+
+            typename CS::T_Axis const h = *particle[smoothingLength];
+            typename CS::T_Axis const m_i = *particle[mass];
+
+            *particle[density] = m_i * KernelT::W(typename CS::T_Axis{0}, h);
+        }
+    };
+
+    /**
+     * Stage functor: full density summation pass.
+     *
+     * Seeds each particle with its self-contribution, then accumulates
+     * neighbour contributions via the pairwise pass.
+     */
+    template<SphKernel KernelT>
+    struct UpdateDensity
+    {
+        void operator()(auto& prBuf, auto const& neighbourRegions, auto const& regionOffsets, typename CS::T_Axis h0)
+            const
+        {
+            pmacc::spearhed::ForEachParticleInPRBuf{}(prBuf, DensityInitSelf<KernelT>{});
+            pmacc::spearhed::InteractParticles{}(
+                prBuf,
+                neighbourRegions,
+                regionOffsets,
+                static_cast<typename CS::T_Axis>(KernelT::supportRadius) * h0,
+                AccumulateDensity<KernelT>{});
+        }
+    };
+
+} // namespace spearhed
---
+++ tests/spearhed/unit/DensitySummation/DensitySummation.cpp
@@ -0,0 +1,355 @@
+/* Copyright 2025-2026 Tapish Narwal
+ *
+ * This file is part of SPEARHED.
+ *
+ * SPEARHED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPEARHED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SPEARHED.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Unit test for SPH density summation.
+ *
+ * Strategy: place N particles all at the SAME point in a single region.
+ * With spacing zero, every pair has r = 0, so W(r,h) = W(0,h) = sigma/h^dim.
+ * Expected density for each particle = N * m * W(0, h).
+ *
+ * This fully exercises both DensityInitSelf (self term) and AccumulateDensity
+ * (pairwise neighbours), and gives an exact analytic result independent of
+ * spatial configuration.
+ */
+
+#include "spearhed/sph/DensitySummation.hpp"
+
+#include "TestSetup.hpp"
+#include "spearhed/ParticleDefinition.hpp"
+#include "spearhed/param.hpp"
+#include "spearhed/particles/attributes/Density.hpp"
+#include "spearhed/particles/attributes/Mass.hpp"
+#include "spearhed/particles/attributes/SmoothingLength.hpp"
+#include "spearhed/particles/initialization/InitParticles.hpp"
+#include "spearhed/sph/CubicSplineKernel.hpp"
+#include "spearhed/sph/KernelVariant.hpp"
+#include "spearhed/test/SpearhedParticleFixture.hpp"
+#include "spmacc/particles/algorithms/ForEachParticle.hpp"
+#include "spmacc/particles/algorithms/ParticleParticleInteraction.hpp"
+
+#include <pmacc/attribute/FunctionSpecifier.hpp>
+#include <pmacc/memory/buffers/HostDeviceBuffer.hpp>
+#include <pmacc/particles/memory/buffers/MallocMCBuffer.hpp>
+#include <pmacc/test/PMaccFixture.hpp>
+
+#include <cmath>
+#include <cstdint>
+
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+static constexpr unsigned TEST_DIM = spearhed::simDim;
+
+using ParticleFixture = spearhed::test::SpearhedParticleFixture<TEST_DIM>;
+
+namespace
+{
+    constexpr spearhed::Real TEST_H = spearhed::Real{0.5};
+    constexpr spearhed::Real TEST_MASS = spearhed::Real{1.0};
+
+    /**
+     * Initialises each particle: place all at AABB centre, set mass and smoothingLength.
+     */
+    struct InitDensityTestParticle
+    {
+        DINLINE constexpr void operator()(
+            auto const& /*worker*/,
+            auto& particle,
+            auto const& particleRegion,
+            uint32_t /*globalParticleIdx*/) const
+        {
+            using namespace pmacc::spearhed::tags;
+            using namespace spearhed::tags;
+
+            auto const& aabb = particleRegion.volume;
+            // All particles at AABB centre
+            pmacc::spearhed::for_each_tag<spearhed::CS>(
+                [&](auto tag)
+                { *particle[relativePos][tag] = (aabb.min[tag] + aabb.max[tag]) * spearhed::Real{0.5}; });
+
+            *particle[mass] = TEST_MASS;
+            *particle[smoothingLength] = TEST_H;
+            *particle[density] = spearhed::Real{0};
+        }
+    };
+
+    struct InitDensityTestSetup
+    {
+        pmacc::spearhed::AABB<spearhed::CS> domain{{0, 0, 0}, {-1.0, -1.0, -1.0}, {1.0, 1.0, 1.0}};
+
+        static constexpr uint32_t N = 4u;
+
+        struct NumParticlesToCreate
+        {
+            constexpr auto operator()(auto& /*worker*/, auto& /*region*/, uint32_t n) const
+            {
+                return n;
+            }
+        };
+
+        auto numParticlesToCreateArgs() const
+        {
+            return std::make_tuple(N);
+        }
+
+        struct PlaceParticle
+        {
+            DINLINE constexpr void operator()(
+                auto const& worker,
+                auto& particle,
+                auto const& particleRegion,
+                uint32_t globalParticleIdx) const
+            {
+                InitDensityTestParticle{}(worker, particle, particleRegion, globalParticleIdx);
+            }
+        };
+
+        spearhed::KernelVariant kernelVariant = makeKernel(spearhed::KernelType::CubicSpline);
+
+        auto placeParticleArgs() const
+        {
+            return std::make_tuple();
+        }
+
+        template<typename PRBuf, typename DeviceHeapT>
+        void setupRegions(PRBuf& prBuf, DeviceHeapT const& deviceHeap) const
+        {
+            using PRType = typename PRBuf::ParticleRegionType;
+            prBuf.create(1);
+            auto deviceHeapHandle = deviceHeap.getAllocatorHandle();
+            auto region = PRType{deviceHeapHandle, {{0, 0, 0}, {-1.0, -1.0, -1.0}, {1.0, 1.0, 1.0}}};
+            prBuf.pushBack(region);
+            prBuf.buffer->hostToDevice();
+        }
+    };
+
+    // Three particles spaced along x-axis: dx < 2h so adjacent pairs interact,
+    // 2*dx > 2h so non-adjacent pair does not.
+    constexpr spearhed::Real SPACED_H = spearhed::Real{1.0};
+    constexpr spearhed::Real SPACED_MASS = spearhed::Real{1.5};
+    constexpr spearhed::Real SPACED_DX = spearhed::Real{1.5}; // dx=1.5 < 2h=2.0 < 2*dx=3.0
+
+    struct InitSpacedParticle
+    {
+        DINLINE constexpr void operator()(
+            auto const& /*worker*/,
+            auto& particle,
+            auto const& /*particleRegion*/,
+            uint32_t globalParticleIdx) const
+        {
+            using namespace pmacc::spearhed::tags;
+            using namespace spearhed::tags;
+
+            // particle i placed at x = i*dx, y=z=0
+            pmacc::spearhed::for_each_enum_tag<spearhed::CS>(
+                [&](auto axisIdx, auto tag)
+                {
+                    *particle[relativePos][tag]
+                        = (axisIdx.value == 0) ? spearhed::Real(globalParticleIdx) * SPACED_DX : spearhed::Real{0};
+                });
+
+            *particle[mass] = SPACED_MASS;
+            *particle[smoothingLength] = SPACED_H;
+            *particle[density] = spearhed::Real{0};
+        }
+    };
+
+    struct InitSpacedTestSetup
+    {
+        pmacc::spearhed::AABB<spearhed::CS> domain{{0, 0, 0}, {-5.0, -5.0, -5.0}, {5.0, 5.0, 5.0}};
+
+        static constexpr uint32_t N = 3u;
+
+        struct NumParticlesToCreate
+        {
+            constexpr auto operator()(auto& /*worker*/, auto& /*region*/, uint32_t n) const
+            {
+                return n;
+            }
+        };
+
+        auto numParticlesToCreateArgs() const
+        {
+            return std::make_tuple(N);
+        }
+
+        struct PlaceParticle
+        {
+            DINLINE constexpr void operator()(
+                auto const& worker,
+                auto& particle,
+                auto const& particleRegion,
+                uint32_t globalParticleIdx) const
+            {
+                InitSpacedParticle{}(worker, particle, particleRegion, globalParticleIdx);
+            }
+        };
+
+        spearhed::KernelVariant kernelVariant = makeKernel(spearhed::KernelType::CubicSpline);
+
+        auto placeParticleArgs() const
+        {
+            return std::make_tuple();
+        }
+
+        template<typename PRBuf, typename DeviceHeapT>
+        void setupRegions(PRBuf& prBuf, DeviceHeapT const& deviceHeap) const
+        {
+            using PRType = typename PRBuf::ParticleRegionType;
+            prBuf.create(1);
+            auto deviceHeapHandle = deviceHeap.getAllocatorHandle();
+            auto region = PRType{deviceHeapHandle, {{0, 0, 0}, {-5.0, -5.0, -5.0}, {5.0, 5.0, 5.0}}};
+            prBuf.pushBack(region);
+            prBuf.buffer->hostToDevice();
+        }
+    };
+
+} // namespace
+
+TEST_CASE_METHOD(
+    ParticleFixture,
+    "DensitySummation: N co-located particles each have density N*m*W(0,h)",
+    "[sph][density]")
+{
+    auto setup = InitDensityTestSetup{};
+    setup.setupRegions(*prBuf, *deviceHeap);
+    spearhed::InitParticles{}(setup);
+
+    // All-to-all neighbour graph (single region)
+    constexpr int numRegions = 1;
+    pmacc::HostDeviceBuffer<int, 1> neighbourRegions(numRegions * numRegions);
+    pmacc::HostDeviceBuffer<int, 1> regionOffsets(numRegions + 1);
+    neighbourRegions.getHostBuffer().data()[0] = 0;
+    regionOffsets.getHostBuffer().data()[0] = 0;
+    regionOffsets.getHostBuffer().data()[1] = 1;
+    neighbourRegions.hostToDevice();
+    regionOffsets.hostToDevice();
+
+
+    std::visit(
+        [&](auto kernel)
+        {
+            using K = std::decay_t<decltype(kernel)>;
+            // Self-contribution first, then pairwise accumulation
+            pmacc::spearhed::ForEachParticleInPRBuf{}(*prBuf, spearhed::DensityInitSelf<K>{});
+            pmacc::spearhed::InteractParticles{}(
+                *prBuf,
+                neighbourRegions,
+                regionOffsets,
+                spearhed::Real{2} * TEST_H,
+                spearhed::AccumulateDensity<K>{});
+        },
+        setup.kernelVariant);
+
+
+    // Read densities back to host
+    prBuf->buffer->deviceToHost();
+    auto hostRegions = prBuf->buffer->getHostBuffer().getDataBox();
+    auto& frameList = hostRegions(0).particleFrameList;
+
+    spearhed::Real const expected
+        = spearhed::Real(InitDensityTestSetup::N) * TEST_MASS * spearhed::CubicSplineKernel::W(0.0f, TEST_H);
+
+    uint32_t checkedCount = 0;
+    for(auto& frame : frameList)
+    {
+        for(uint32_t slot = 0; slot < spearhed::numFrameSlots; ++slot)
+        {
+            auto particle = frame[slot];
+            if(*particle[pmacc::spearhed::tags::multiMask])
+            {
+                spearhed::Real const rho = *particle[spearhed::tags::density];
+                REQUIRE(static_cast<double>(rho) == Catch::Approx(static_cast<double>(expected)).epsilon(1e-5));
+                ++checkedCount;
+            }
+        }
+    }
+    REQUIRE(checkedCount == InitDensityTestSetup::N);
+}
+
+TEST_CASE_METHOD(
+    ParticleFixture,
+    "DensitySummation: 3 particles in a line have correct neighbour-dependent densities",
+    "[sph][density]")
+{
+    auto setup = InitSpacedTestSetup{};
+    setup.setupRegions(*prBuf, *deviceHeap);
+    spearhed::InitParticles{}(setup);
+
+    constexpr int numRegions = 1;
+    pmacc::HostDeviceBuffer<int, 1> neighbourRegions(numRegions * numRegions);
+    pmacc::HostDeviceBuffer<int, 1> regionOffsets(numRegions + 1);
+    neighbourRegions.getHostBuffer().data()[0] = 0;
+    regionOffsets.getHostBuffer().data()[0] = 0;
+    regionOffsets.getHostBuffer().data()[1] = 1;
+    neighbourRegions.hostToDevice();
+    regionOffsets.hostToDevice();
+
+    std::visit(
+        [&](auto kernel)
+        {
+            using K = std::decay_t<decltype(kernel)>;
+            pmacc::spearhed::ForEachParticleInPRBuf{}(*prBuf, spearhed::DensityInitSelf<K>{});
+            pmacc::spearhed::InteractParticles{}(
+                *prBuf,
+                neighbourRegions,
+                regionOffsets,
+                spearhed::Real{2} * SPACED_H,
+                spearhed::AccumulateDensity<K>{});
+        },
+        setup.kernelVariant);
+
+
+    prBuf->buffer->deviceToHost();
+    auto hostRegions = prBuf->buffer->getHostBuffer().getDataBox();
+    auto& frameList = hostRegions(0).particleFrameList;
+
+    // rho_edge: self + one neighbour at distance dx
+    spearhed::Real const rho_edge = SPACED_MASS
+                                    * (spearhed::CubicSplineKernel::W(spearhed::Real{0}, SPACED_H)
+                                       + spearhed::CubicSplineKernel::W(SPACED_DX, SPACED_H));
+    // rho_mid: self + two neighbours at distance dx
+    spearhed::Real const rho_mid = SPACED_MASS
+                                   * (spearhed::CubicSplineKernel::W(spearhed::Real{0}, SPACED_H)
+                                      + spearhed::Real{2} * spearhed::CubicSplineKernel::W(SPACED_DX, SPACED_H));
+
+    uint32_t countEdge = 0;
+    uint32_t countMid = 0;
+    for(auto& frame : frameList)
+    {
+        for(uint32_t slot = 0; slot < spearhed::numFrameSlots; ++slot)
+        {
+            auto particle = frame[slot];
+            if(*particle[pmacc::spearhed::tags::multiMask])
+            {
+                spearhed::Real const rho = *particle[spearhed::tags::density];
+                double const rho_d = static_cast<double>(rho);
+                if(Catch::Approx(rho_d).epsilon(1e-5) == static_cast<double>(rho_edge))
+                    ++countEdge;
+                else if(Catch::Approx(rho_d).epsilon(1e-5) == static_cast<double>(rho_mid))
+                    ++countMid;
+                else
+                    FAIL("Unexpected density value: " << rho_d);
+            }
+        }
+    }
+    REQUIRE(countEdge == 2u);
+    REQUIRE(countMid == 1u);
+}
